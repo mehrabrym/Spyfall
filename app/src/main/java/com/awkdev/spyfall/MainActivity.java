fd9cecaa7553a31dbd34eb90754321efc8294c3a
Rayun Mehrab <rayun.mehrab@mail.utoronto.ca>
2015-06-13 00:55:30 -0400

    Adding all the current files for the app

---
+++ app/src/main/java/com/awkdev/spyfall/MainActivity.java
@@ -0,0 +1,14 @@
+package com.awkdev.spyfall;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
