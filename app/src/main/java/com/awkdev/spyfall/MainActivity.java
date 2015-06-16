01f669cd8f786451b7b04e661bdcdc2f01dd963d
Rayun Mehrab <rayun.mehrab@mail.utoronto.ca>
2015-06-15 22:56:10 -0400

    More edits to get action bar material themed.

--- app/src/main/java/com/awkdev/spyfall/MainActivity.java
+++ app/src/main/java/com/awkdev/spyfall/MainActivity.java
@@ -2,13 +2,19 @@ package com.awkdev.spyfall;
 
 import android.app.*;
 import android.os.*;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends Activity 
+public class MainActivity extends ActionBarActivity 
 {
+	Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main_activity);
+		
+		toolbar = (Toolbar) findViewById(R.id.maintoolbar);
+        setSupportActionBar(toolbar);
     }
 }
